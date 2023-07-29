8801d065ec3987f2f9cf1b86416ac3e6cdd5c7f2
omarmamado <omarelmageco12@gmail.com>
2023-07-29 23:31:35 +0200

    Unlimited subcategories

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('menus','App\Http\Controllers\MenuController@index');
+Route::get('menus-show','App\Http\Controllers\MenuController@show');
+Route::post('menus','App\Http\Controllers\MenuController@store')->name('menus.store');
+
+
+
+
